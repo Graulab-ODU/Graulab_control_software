0f7b740120069e48f59ae29d81e62bac25c4d079
JoshWager1 <josh.wager22@gmail.com>
2023-06-13 16:41:25 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='moglabs_fzw',
+    version='0.0.1',
+    description='driver program for the mogDevice wavemeter',
+    url='https://github.com/Graulab-ODU/Wavemeter_driver',
+    author='Joshua Wager',
+    author_email='josh.wager22@gmail.com',
+    packages=['moglabs_fzw']
+
+)
